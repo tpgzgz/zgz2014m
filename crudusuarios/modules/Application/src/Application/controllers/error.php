02c161314ca3107951b42f545b3efa1fea117b1b
Antonio Blasco Cerrada <814ck.c0d3@gmail.com>
2014-12-03 21:51:02 +0100

    fix http_response_code
    fix http_response_code

--- crudusuarios/modules/Application/src/Application/controllers/error.php
+++ crudusuarios/modules/Application/src/Application/controllers/error.php
@@ -3,9 +3,11 @@
 switch ($request['action'])
 {
     case '404':
+        http_response_code(404);
         echo "Error 404: Por más que busco, no encuentro.";
     break;
     case '405':
+        http_response_code(405);
         echo "Error 405: La dirección está muy mal puesta";
     break;
 }
