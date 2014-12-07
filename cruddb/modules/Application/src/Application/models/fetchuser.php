17c2d98e46f67710f12dc6b4bedc327eb63abd77
Antonio Blasco Cerrada <814ck.c0d3@gmail.com>
2014-12-07 05:56:20 +0100

    updated for db and unificated variables names

--- cruddb/modules/Application/src/Application/models/fetchuser.php
+++ cruddb/modules/Application/src/Application/models/fetchuser.php
@@ -3,18 +3,36 @@
 /**
  * Fetch user by id
  * @param int $id
- * @return array
+ * @param array $config
+ * @return array $user
  */
-function fetchUser($id)
+function fetchUser($id,$config)
 {
-    // Leer los datos del usuario por ID
-    // Leer todos los datos
-    $data = file_get_contents($_SERVER['DOCUMENT_ROOT']."/usuarios.txt");
-    // Dividir por saltos de linea
-    $data = explode("\n", $data);
-    // Leer la fila ID
-    $usuario = $data[$id];
-    $usuario = explode("|", $usuario);
-    
-    return $usuario;
+    switch ($config['repository'])
+    {
+        case 'txt':
+            // Leer los datos del usuario por ID
+            // Leer todos los datos
+            $user = file_get_contents($_SERVER['DOCUMENT_ROOT']."/usuarios.txt");
+            // Dividir por saltos de linea
+            $user = explode("\n", $user);
+            // Leer la fila ID
+            $user = $user[$id];
+            $user = explode("|", $user);
+          
+            return $user;
+            break;
+        case 'db':
+            // Conectarse al DBMS
+            $link = mysqli_connect($config['database']['host'],
+                $config['database']['user'],
+                $config['database']['password']);
+            // Seleccionar la DB
+            mysqli_select_db($link, $config['database']['database']);
+            // Seleccionar el usuario
+            $sql = "SELECT * FROM users WHERE iduser = " . $id;
+            $user = mysqli_query($link, $sql);
+            return mysqli_fetch_assoc($user);
+            break;
+    }
 }
