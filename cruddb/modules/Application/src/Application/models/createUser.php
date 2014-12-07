3042ec4b609c29a4a5df7f8ed8e214bf61a52898
Antonio Blasco Cerrada <814ck.c0d3@gmail.com>
2014-12-07 07:09:02 +0100

    updated for db need to config db row names

--- cruddb/modules/Application/src/Application/models/createUser.php
+++ cruddb/modules/Application/src/Application/models/createUser.php
@@ -8,27 +8,69 @@
  * 
  * @param array $filter
  * @param string $imagename
- * @param string $filename
- * @param boolean $append 
+ * @param array $config
+ * @param userForm $user
  * @return number int number of bytes | FALSE
  */
 
-function createUser($filter, $imagename)
+function createUser($filter, $imagename, $config, $user)
 {
-    $filename = 'usuarios.txt';
-    
-    foreach($filter as $key => $value)
+    switch ($config['repository'])
     {
-        if(is_array($value))
-            $value=implode(',', $value);
-        $data[$key]=$value;
+        case 'txt';
+            $filename = 'usuarios.txt';
+            
+            foreach($filter as $key => $value)
+            {
+                if(is_array($value))
+                    $value=implode(',', $value);
+                $data[$key]=$value;
+            }
+            $data[]=$imagename;
+            $data = implode('|', $data);
+            return file_put_contents($_SERVER['DOCUMENT_ROOT']."/".$filename, $data."\n",FILE_APPEND);
+            break;
+        case 'db':
+//             // Conectarse al DBMS
+//             $link = mysqli_connect($config['database']['host'],
+//                 $config['database']['user'],
+//                 $config['database']['password']);
+//             // Seleccionar la DB
+//             mysqli_select_db($link, $config['database']['database']);
+//             //selecciona el id de ciudad
+//             $sql = "SELECT idcity FROM cities JOIN users WHERE city ='".$user['city']."'";
+//             $user['city'] = mysqli_query($link, $sql);
+            
+//             $sql = "SELECT idgender FROM genders JOIN users WHERE gender ='".$user['gender']."'";
+//             $user['gender'] = mysqli_query($link, $sql);
+//             // crear el usuario el usuario
+//             $sql = "Insert INTO users SET
+//                                 id = '".$user['id']."',
+//                                 name = '".$user['name']."',
+//                                 lastname = '".$user['lastname']."',
+//                                 password = '".$user['password']."',
+//                                 description = '".$user['description']."',
+//                                 gender = '".$user['gender']."',
+//                                 city = '".$user['city']."'";
+//             // crear el resto de relaciones
+//             foreach($user['pet'] as $pet)
+//             {
+//                 $sql = "SELECT idpet FROM pets JOIN users WHERE pet ='".$pet."'";
+//                 $pet = mysqli_query($link, $sql);
+//                 $sql = "INSERT INTO user_has_pet SET
+//                                 idpet = '".$pet."',
+//                                 iduser = '".$user['id']."'";
+//                 mysqli_query($link, $sql);
+//             }
+//             foreach($user['language'] as $lang)
+//             {
+//                 $sql = "SELECT idlanguage FROM languages JOIN users WHERE language ='".$lang."'";
+//                 $lang = mysqli_query($link, $sql);
+//                 $sql = "INSERT INTO user_has_pet SET
+//                                 idpet = '".$lang."',
+//                                 iduser = '".$user['id']."'";
+//                 mysqli_query($link, $sql);
+//             }
+            break;
     }
-    $data[]=$imagename;
-    $data = implode('|', $data);
-    
-        return file_put_contents($_SERVER['DOCUMENT_ROOT']."/".$filename,
-                      $data."\n",
-                      FILE_APPEND);
-    
-     
 }
