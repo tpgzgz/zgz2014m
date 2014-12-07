37730cac937098a979a8ab1405e043f8ea2a31f7
Antonio Blasco Cerrada <814ck.c0d3@gmail.com>
2014-12-07 07:19:03 +0100

    Masked for switch txt/db

--- cruddb/modules/Application/src/Application/models/updateUser.php
+++ cruddb/modules/Application/src/Application/models/updateUser.php
@@ -1,30 +1,39 @@
 <?php
 /**
  * Update data of an user
- * 
+ *
  * @param array $filter
+ * @param array $config
+ * @param userForm $user
  * @return nothing just change data on repository
  */
- 
-function updateUser($filter)
+
+function updateUser($filter, $config, $user)
 {
-    $filename = 'usuarios.txt';
-    // Recorrer el array de datos
-    $usuarios = file_get_contents($_SERVER['DOCUMENT_ROOT']."/".$filename);
-    // Dividir por saltos de linea
-    $usuarios = explode("\n", $usuarios);
-    foreach($filter as $key => $value)
+    switch ($config['repository'])
     {
-        if(is_array($value))
-            $value=implode(',', $value);
-        $data[$key]=$value;
+        case 'txt':
+            $filename = 'usuarios.txt';
+            // Recorrer el array de datos
+            $usuarios = file_get_contents($_SERVER['DOCUMENT_ROOT']."/".$filename);
+            // Dividir por saltos de linea
+            $usuarios = explode("\n", $usuarios);
+            foreach($filter as $key => $value)
+            {
+                if(is_array($value))
+                    $value=implode(',', $value);
+                $data[$key]=$value;
+            }
+            $data = implode('|', $data);
+            
+            $usuarios[$filter['id']] = $data;
+            $usuarios = implode("\n", $usuarios);
+            // Escribir todo el array al fichero
+            return file_put_contents($_SERVER['DOCUMENT_ROOT']."/usuarios.txt",
+            $usuarios);
+            break;
+        case 'db':
+            
+            break;
     }
-    $data = implode('|', $data);
-    
-    $usuarios[$filter['id']] = $data;
-    $usuarios = implode("\n", $usuarios);
-    // Escribir todo el array al fichero
-    return file_put_contents($_SERVER['DOCUMENT_ROOT']."/usuarios.txt",
-    $usuarios);
-     
 }
